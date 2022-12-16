6a72c9c5501c6eea613985046308f3abb950f2eb
bouchardf <112488668+bouchardf@users.noreply.github.com>
2022-12-16 16:02:29 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-class StringFoo():
-    def __init__(self):
-        self.string = "ce text devrait être en majuscule"
-    def set_string(self, string):
-        self.string = string
-    def print_string(self):
-        print(self.string.upper())
-
-StringFoo().print_string()
